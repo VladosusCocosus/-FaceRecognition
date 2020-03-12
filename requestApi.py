82cd1aa8d2272fe4b1007d10edda04e9052ae40d
vladosus <razin36986@gmail.ru>
2020-03-12 22:20:45 +0500

    commit-2

--- requestApi.py
+++ requestApi.py
@@ -1,8 +1,6 @@
 from app import app
 import asyncio, io, glob, os, sys, time, uuid, requests
 from urllib.parse import urlparse
-from io import BytesIO
-from PIL import Image, ImageDraw
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
